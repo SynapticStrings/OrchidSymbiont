40a437ef9313bbe823b546e0c0c08aae556fca90
RoastedChestnut <chestnut.roasted@outlook.com>
2026-01-02 21:58:08 +0800

    chore(deps): update orchid dependency to v0.5.0 and bump copyright year
    Updated the `orchid` dependency from version 0.4.0 to 0.5.0 to leverage new features and improvements. Also updated the copyright year in the LICENSE file from 2025 to 2026 to reflect the current year.

--- examples/simple_model.ex
+++
@@ -1,9 +0,0 @@
-defmodule DummyStep do
-  @behaviour Orchid.Symbiont.Step
-
-  def required, do: []
-
-  def run_with_model(_inputs, _symbiont_map, _opts) do
-    # ...
-  end
-end
--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule OrchidSymbiont.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:orchid, "~> 0.4"},
+      {:orchid, "~> 0.5"},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false}
     ]
   end
